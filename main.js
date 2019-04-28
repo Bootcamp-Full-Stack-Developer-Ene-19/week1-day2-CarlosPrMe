6d7d6bae94e299bd9876cfe5e247eb13791e6816
Carlos Prieto <carlospm86@gmail.com>
2019-04-28 16:30:54 +0200

    correciones responsive

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     $('.search-small__btn').click(function(){
         $('.search-small').addClass('search-small--open');
         $('.navbar__toggler').addClass('navbar__toggler--hidden');
-
     })
     $('.search-small__close').click(function(){
         $('.search-small').removeClass('search-small--open');
@@ -11,9 +10,11 @@ $(document).ready(function() {
     })
 
     $('.navbar__toggler').click(function () {
-        $('.navbar').addClass('show'); 
+        $('nav.navbar').addClass('show'); 
+        $('body').addClass('hide');
     })
     $('.search-small__close').click(function () {
         $('.navbar').removeClass('show');
+        $('body').removeClass('hide');
     })
 })
